a09a4b852f70208af8d18f638bc9a88e098405e8
isawu888 <isawu888@login005.inband>
2026-04-24 16:20:36 -0400

    fix HuggingFace repo ID for weight download

--- watermark/download_weights.py
+++ watermark/download_weights.py
@@ -1,19 +1,15 @@
-"""
-Run this script once to download all pretrained weights from HuggingFace.
-Usage: python watermark/download_weights.py
-"""
 from huggingface_hub import hf_hub_download
 
 print("Downloading VQ-VAE tokenizer...")
 hf_hub_download(
-    repo_id="FoundationVision/vq_ds16_c2i",
+    repo_id="FoundationVision/LlamaGen",
     filename="vq_ds16_c2i.pt",
     local_dir="./pretrained_models"
 )
 
 print("Downloading LlamaGen-L weights...")
 hf_hub_download(
-    repo_id="FoundationVision/LlamaGen-L",
+    repo_id="FoundationVision/LlamaGen",
     filename="c2i_L_384.pt",
     local_dir="./pretrained_models"
 )
