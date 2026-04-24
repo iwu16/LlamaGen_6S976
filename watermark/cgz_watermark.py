eee17987a503bb3a8e1ca7cab4b92ab32ad4e759
Isabella <isawu888@mit.edu>
2026-04-24 15:45:45 -0400

    add watermark module scaffold

---
+++ watermark/cgz_watermark.py
@@ -0,0 +1,8 @@
+"""
+CGZ watermarking scheme adapted for LlamaGen.
+Reference: Christ, Gunn, Zamir (COLT 2024)
+"""
+
+# TODO: implement watermark injection into LlamaGen sampling loop
+# TODO: implement watermark detector
+
---
+++ watermark/metrics.py
@@ -0,0 +1,8 @@
+"""
+Evaluation metrics: FID, LPIPS, TPR/FPR.
+"""
+
+# TODO: implement FID computation
+# TODO: implement LPIPS scoring
+# TODO: implement detection accuracy (TPR/FPR)
+
