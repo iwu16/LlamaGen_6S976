0e3167bec2db9f0a6387d5758cabe6cbce9528cb
isawu888 <isawu888@login005.inband>
2026-04-24 16:57:31 -0400

    Fixed generation slurm script

--- run_generate.sh
+++ run_generate.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 #SBATCH --job-name=cgz_generate
+#SBATCH --partition=mit_normal_gpu
 #SBATCH --gres=gpu:1
 #SBATCH --mem=32G
-#SBATCH --time=24:00:00
+#SBATCH --time=06:00:00
 #SBATCH --output=logs/generate_%j.out
 
 conda activate llamagen
